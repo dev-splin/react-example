4450f93d2726965f83f2f870a576433dcf0e6b03
dev-splin <dev.splin@gmail.com>
2022-12-28 23:31:03 +0900

    ADD : [react] 프로젝트 생성, input 상태 관리

---
+++ contact-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PhoneForm from "./components/PhoneForm";
+
+function App() {
+  return (
+    <div>
+      <PhoneForm></PhoneForm>
+    </div>
+  );
+}
+
+export default App;
